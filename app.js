9c8a1da75d8d83a625562f62dd440924697dc69a
tariqansari <36394438+tariqansari@users.noreply.github.com>
2018-05-16 12:41:30 +0530

    update port number

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ var restify = require('restify');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(8085, function() {
-
-  console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot and listen to messages
